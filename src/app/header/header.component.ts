69ce8e6fec05959c6a67e6a82ea82413bbfaf090
IshakAtes <ishakfeuer@gmail.com>
2024-10-29 23:44:24 +0100

    added carousel

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgFor],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+
+  slideshowImages: string[] = [
+    'assets/img/slide1.jpg',
+    'assets/img/slide2.jpg',
+    'assets/img/slide3.jpg',
+    'assets/img/slide4.jpg',
+    'assets/img/slide5.jpg'
+  ];
+
+  currentIndex: number = 0;
+  intervalId: any;
+
+  ngOnInit() {
+    this.startSlideshow();
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  startSlideshow() {
+    this.intervalId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.slideshowImages.length;
+    }, 5000); // 5000 ms = 5 Sekunden
+  }
 
 }
