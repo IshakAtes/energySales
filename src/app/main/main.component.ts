7be6a516e1d34a8fa0e48fd4f36dba270e28215a
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-10-30 13:40:37 +0100

    added our-service text and service-banner component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,11 +3,12 @@ import { NavComponent } from '../nav/nav.component';
 import { HeaderComponent } from '../header/header.component';
 import { CallToMeetingComponent } from "../call-to-meeting/call-to-meeting.component";
 import { OurServiceComponent } from "../our-service/our-service.component";
+import { ServiceBannerComponent } from "../service-banner/service-banner.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [NavComponent, HeaderComponent, CallToMeetingComponent, OurServiceComponent],
+  imports: [NavComponent, HeaderComponent, CallToMeetingComponent, OurServiceComponent, ServiceBannerComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
---
+++ src/app/service-banner/service-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './service-banner.component.html',
+  styleUrl: './service-banner.component.scss'
+})
+export class ServiceBannerComponent {
+
+}
