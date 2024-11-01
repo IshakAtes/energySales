cc89ad76ec1aaf8558763136b9791fdf3167140a
IshakAtes <ishakfeuer@gmail.com>
2024-11-01 11:38:38 +0100

    added contact component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from '../header/header.component';
 import { CallToMeetingComponent } from "../call-to-meeting/call-to-meeting.component";
 import { OurServiceComponent } from "../our-service/our-service.component";
 import { ServiceBannerComponent } from "../service-banner/service-banner.component";
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [NavComponent, HeaderComponent, CallToMeetingComponent, OurServiceComponent, ServiceBannerComponent],
+  imports: [NavComponent, HeaderComponent, CallToMeetingComponent, OurServiceComponent, ServiceBannerComponent, ContactComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
