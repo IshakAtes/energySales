4041b61fe08c0f70695eec4d10b98a46db9c2f87
IshakAtes <ishakfeuer@gmail.com>
2024-11-09 14:43:33 +0100

    added roadmap component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,11 +6,12 @@ import { OurServiceComponent } from "../our-service/our-service.component";
 import { ServiceBannerComponent } from "../service-banner/service-banner.component";
 import { ContactComponent } from '../contact/contact.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RoadmapComponent } from '../roadmap/roadmap.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [NavComponent, HeaderComponent, CallToMeetingComponent, OurServiceComponent, ServiceBannerComponent, ContactComponent, FooterComponent],
+  imports: [NavComponent, HeaderComponent, CallToMeetingComponent, RoadmapComponent, OurServiceComponent, ServiceBannerComponent, ContactComponent, FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
