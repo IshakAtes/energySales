4cb1b946c254ce53e07ed088d1428bb02b8a22ec
IshakAtes <ishakfeuer@gmail.com>
2024-11-09 19:04:32 +0100

    added footer banner

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ContactComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
