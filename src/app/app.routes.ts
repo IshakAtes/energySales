0a4638c7374d5c5e4ced7e6e3684a894f3332d55
IshakAtes <ishakfeuer@gmail.com>
2024-11-09 18:17:49 +0100

    added imprint, privacy-policy and credits-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { CreditsComponent } from './credits/credits.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
+    { path: 'credits', component: CreditsComponent }
 ];
