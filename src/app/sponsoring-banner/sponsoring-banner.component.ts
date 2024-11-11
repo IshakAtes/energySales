c901415e2f82cafd03d4211be04ab3ae0a0c66e2
IshakAtes <ishakfeuer@gmail.com>
2024-11-11 16:24:41 +0100

    added images in carousel-container

--- src/app/sponsoring-banner/sponsoring-banner.component.ts
+++ src/app/sponsoring-banner/sponsoring-banner.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sponsoring-banner',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './sponsoring-banner.component.html',
   styleUrl: './sponsoring-banner.component.scss'
 })
 export class SponsoringBannerComponent {
-
+  imageIndices = Array.from({ length: 50 }, (_, index) => index);
 }
