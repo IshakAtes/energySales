48177c6ad289d69697396658ee7a576b88007c8e
IshakAtes <ishakfeuer@gmail.com>
2024-11-25 19:25:28 +0100

    button disabled, form not valid

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,38 +1,32 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormBuilder, FormControl, NgModel, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
   selectControl:FormControl = new FormControl()
   myForm = this.formBuilder.group({
-    name: '',
-    lastName: '',
-    phoneNumber: '',
-    power: this.selectControl,
-    email: '',
-    consume: '',
-    feedback: ''
+    name: ['', [Validators.required, Validators.minLength(2)]],
+    lastName: ['', [Validators.required, Validators.minLength(2)]],
+    phoneNumber: ['', [Validators.required]],
+    power: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    consume: ['', Validators.required],
+    feedback: ['']
   });
-  // myForm: formModel = this.formBuilder.group({
-  //   name: '',
-  //   lastName: '',
-  //   phoneNumber: '',
-  //   power: this.selectControl,
-  //   email: '',
-  //   consume: '',
-  //   feedback: ''
-  // })
+
 
   constructor (private formBuilder: FormBuilder) {
   }
 
   onSubmit() {
     console.log('Form submitted', this.myForm.value);
+    this.myForm.reset();
   }
 }
