435798d1bc8fc9679a7406308ae03c9389e0bed3
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-12-02 15:11:09 +0100

    added dialog call to action , but not finished

--- src/app/call-to-meeting/call-to-meeting.component.ts
+++ src/app/call-to-meeting/call-to-meeting.component.ts
@@ -1,12 +1,20 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-call-to-meeting',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './call-to-meeting.component.html',
   styleUrl: './call-to-meeting.component.scss'
 })
 export class CallToMeetingComponent {
+  toggleDialog: boolean = false;
+
+  constructor() {}
+
+  openDialog() {
+    this.toggleDialog = !this.toggleDialog;
+  }
 
 }
