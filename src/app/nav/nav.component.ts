3a7491476287cace31d260bc9ceff812f81c1c26
IshakAtes <ishakfeuer@gmail.com>
2024-12-06 01:38:36 +0100

    added routerlink

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -4,7 +4,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
