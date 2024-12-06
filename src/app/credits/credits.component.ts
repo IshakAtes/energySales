d722fad4619c7a26cc55558be4ac9b81bcd56c3d
IshakAtes <ishakfeuer@gmail.com>
2024-12-06 01:00:44 +0100

    styled credits component

--- src/app/credits/credits.component.ts
+++ src/app/credits/credits.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../nav/nav.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-credits',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, FooterComponent],
   templateUrl: './credits.component.html',
   styleUrl: './credits.component.scss'
 })
